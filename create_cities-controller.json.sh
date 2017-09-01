4c1411675c540eba443ea91476127e2561d20b3c
Luohua HUANG <luohua.huang@gmail.com>
2017-09-01 20:54:25 +0800

    Update create_cities-controller.json.sh

--- create_cities-controller.json.sh
+++ create_cities-controller.json.sh
@@ -26,7 +26,7 @@ cat > cities-controller.json <<EOF
         "containers": [
           {
             "imagePullPolicy": "Always",
-            "image": "quay.io/wercker/wercker-kubernetes-quay:${WERCKER_GIT_COMMIT}",
+            "image": "quay.io/luhuang/demo:${WERCKER_GIT_COMMIT}",
             "name": "cities",
             "ports": [
               {
