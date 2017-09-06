d5ef77714e26856819b8a210bc69120fb67b9447
luohuahuang <luohua.huang@gmail.com>
2017-09-06 02:05:30 -0700

    changed to shenzhen

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func CityHandler(res http.ResponseWriter, req *http.Request) {
 			"London",
 			"Paris",
 			"Seoul",
-			"Beijing",
+			"Shenzhen",
 		},
 	}
 	data, _ := json.MarshalIndent(citiesResponse, "", "  ")
